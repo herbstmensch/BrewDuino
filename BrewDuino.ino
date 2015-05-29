6d0e1e08bb7c41b723d7b5cbaacb59255c189f55
Tim Herbst <mail@timherbst.de>
2015-05-29 11:54:30 +0200

    Update BrewDuino.ino

--- BrewDuino.ino
+++ BrewDuino.ino
@@ -6,7 +6,7 @@
 #include <LCD5110_Basic.h>
 #include <ClickEncoder.h>
 #include <TimerOne.h>
-
+#include <FiniteStateMachine.h>
 
 //Konfigurationsparameter festlegen
 int tempOffset = 1;
@@ -19,36 +19,38 @@ extern uint8_t SmallFont[];
 ClickEncoder *encoder;
 int16_t lastEncoderValue, encoderValue;
 
-//Zustände festlegen
-#define STATE_MENU 1
-#define STATE_MAISCHEN 2
-#define STATE_KOCHEN 3
-#define STATE_SETTINGS 4
-#define STATE_MAISCHEN_START 0
-#define STATE_MAISCHEN_EMT 1
-#define STATE_MAISCHEN_AR 2
-#define STATE_MAISCHEN_R 3
-#define STATE_MAISCHEN_AMT 4
-#define STATE_KOCHEN_KZ 1 
-#define STATE_KOCHEN_AHG 2
-#define STATE_KOCHEN_HG 3
-#define STATE_KOCHEN_WT 4
-#define STATE_KOCHEN_SKT 5
-#define STATE_KOCHEN_KT 6
+//Main FSM und States definieren
+State stateMenu = State(enterMenu, menu, NULL);
+State stateMaischen = State(enterMaischen, maischen, leaveMaischen);
+State stateKochen = State(enterKochen, kochen, leaveKochen);
+//State stateSettings = State(enterSettings, settings, NULL);
+FSM fsmMain = FSM(stateMenu);
+
+//Maischen FSM und States definieren
+State stateEnterEinmaischTemp = State(enterEinmaischTemp);
+State stateEnterAnzahlRaten = State(enterAnzahlRaten);
+State stateDefineRast = State(defineRast);
+State stateEnterAbmaischTemp = State(enterAbmaischTemp);
+State stateDoMaischen = State(doMaischen);
+FSM fsmMaischen = FSM(stateEnterEinmaischTemp);
+
+//Kochen FSM und States definieren
+State stateEnterKochzeit = State(enterKochzeit);
+State stateEnterAnzahlHopfengaben = State(enterAnzahlHopfengaben);
+State stateDefineHopfengaben = State(defineHopfengaben);
+State stateDoKochen = State(doKochen);
+FSM fsmKochen = FSM(stateEnterKochzeit);
+
+/* 
+//Settings FSM und States definieren
+State enterEinmaischTemp;
+FSM fsmSettings = FSM(enterEinmaischTemp);
+*/
 
 //Variablen initialisieren
-int state = STATE_MENU;
-int selectedMenuEntry = 0;
-int lastSelectedMenuEntry = -1;
-
+int selectedMenuEntry, lastSelectedMenuEntry;
 int temp, lastTemp, sollTemp;
 bool isHeating;
-
-void timerIsr() {
-  encoder->service();
-  readTemperature();
-  checkHeatingStatus();
-}
 
 void setup()   {
   Serial.begin(9600);
@@ -62,7 +64,7 @@ void setup()   {
   lcd.print(COPYRIGHT, LEFT, 32);
   lcd.print(COPYRIGHT2, 42, 40);
   
-  //Encoder und Interruot Setup
+  //Encoder und Interrupt Setup
   encoder = new ClickEncoder(2, 3, 4, 2);
   encoder->setAccelerationEnabled(false);
   Timer1.initialize(1000);
@@ -77,6 +79,53 @@ void setup()   {
 }
 
 void loop() {
+  fsmMain.update();
+  
+  ClickEncoder::Button b = encoder->getButton();
+  if (b != ClickEncoder::Open) {
+    if( b == ClickEncoder::Held ){
+      fsmMain.transitionTo(stateMenu);
+    }
+  } 
+  
+  readTemperature();
+  checkHeatingStatus();
+  
+  addTemperature();
+}
+
+void enterMenu(){
+  selectedMenuEntry = 0;
+  lastMenuEntry = -1;
+  sollTemo = 0;
+}
+
+void enterMaischen(){
+  fsmMaischen.immediateTransitionTo();
+  lcd.clrScr();
+  lcd.print("--Maischen--",CENTER,0);
+}
+
+void leaveMaischen(){
+  sollTemp = 0;
+}
+
+void enterKochen(){
+  sollTemp = 100;
+  fsmMaischen.immediateTransitionTo();
+  lcd.clrScr();
+  lcd.print("--Kochen--",CENTER,0);
+}
+
+void leaveKochen(){
+  sollTemp = 0;
+}
+
+/*void enterSettings(){
+  fsmSettings.immediateTransitionTo();
+}*/
+
+void menu() {
   encoderValue += encoder->getValue();
   if(encoderValue != lastEncoderValue){
     lastEncoderValue = encoderValue;
@@ -86,42 +135,49 @@ void loop() {
     Serial.print("Entry: ");
     Serial.println(selectedMenuEntry);
   }
-    
-  switch(state){
-    case STATE_MENU:
-      menu();
-      break;
-    case STATE_MAISCHEN:
-      maischen();
-      break;
-    case STATE_KOCHEN:
-      kochen();
-      break;
-    case STATE_SETTINGS:
-      settings();
-      break;
+  
+  if(selectedMenuEntry != lastSelectedMenuEntry){
+    lastSelectedMenuEntry = selectedMenuEntry;
+    lcd.clrScr();
+    lcd.print(PROG_NAME" "PROG_VERSION, CENTER, 0);
+    lcd.print("=>", 5, 16+selectedMenuEntry*8);
+    lcd.print("Maischen", 30, 16);
+    lcd.print("Kochen", 30, 24);
+    //lcd.print("Parameter", 30, 28);
   }
   
   ClickEncoder::Button b = encoder->getButton();
   if (b != ClickEncoder::Open) {
     if( b == ClickEncoder::Clicked ){
         if(selectedMenuEntry == 0)
-          state = STATE_MAISCHEN;
+          fsmMain.transitionTo(stateMaischen);
         if(selectedMenuEntry == 1)
-          state = STATE_KOCHEN;
-        if(selectedMenuEntry == 2)
-          state = STATE_SETTINGS;
+          fsmMain.transitionTo(stateKochen);
+       // if(selectedMenuEntry == 2)
+         // fsmMain.transitionTo(stateSettings);
     }
   } 
-  
-  addTemperature();
 }
+
+void maischen(){
+  fsmMaischen.update();
+}
+
+void kochen(){
+  fsmKochen.update();
+}
+
+/*void settings(){
+  fsmSettings.update();
+}*/
 
 void readTemperature(){
   temp = 59+random(1);
 }
 
 void checkHeatingStatus(){
+  //Ein oder ausschalten des Heizelementes.
+  //Evtl. schaltfrequent sicherstellen
   if(isHeating){
     if(temp + tempOffset >= sollTemp)
       turnOffHeating();
@@ -133,17 +189,16 @@ void checkHeatingStatus(){
 
 void turnOffHeating(){
   //Toggle Relais
-  
   isHeating = false;
 }
 
 void turnOnHeating(){
   //Toggle Relais
-  
   isHeating = true;
 }
 
 void addTemperature() {
+  //Wenn die aktuelle Temperatur nciht der angezeigten entspricht, aktualisieren.
   if(temp != lastTemp){
     lastTemp = temp;
     lcd.clrRow(5,80);
@@ -154,84 +209,6 @@ void addTemperature() {
   }
 }
 
-void menu() {
-  if(selectedMenuEntry != lastSelectedMenuEntry){
-  lastSelectedMenuEntry = selectedMenuEntry;
-  lcd.clrScr();
-  lcd.print(PROG_NAME" "PROG_VERSION, CENTER, 0);
-  lcd.print("=>", 5, 12+selectedMenuEntry*8);
-  lcd.print("Maischen", 30, 12);
-  lcd.print("Kochen", 30, 20);
-  lcd.print("Parameter", 30, 28);
-  }
-}
-
-void maischen(){
-  lcd.clrScr();
-  lcd.print("--Maischen--",CENTER,0);
-  
-  int stateMaischen = STATE_MAISCHEN_EMT;
-  
-  while(state == STATE_MAISCHEN){
-    switch(stateMaischen){
-      case STATE_MAISCHEN_EMT: //Einmaischtemp festlegen
-        break;
-      case STATE_MAISCHEN_AR: //Anzahl Rasten festlegen
-        break;
-      case STATE_MAISCHEN_R: //Rasten festlegen
-        break;
-      case STATE_MAISCHEN_AMT: //Abmaischtemperatur festlegen
-        break;
-      case STATE_MAISCHEN_WEMT: //Warte auf Abmaischtemperatur
-        break;
-    } 
-    
-    ClickEncoder::Button b = encoder->getButton();
-    if (b != ClickEncoder::Open) {
-      if( b == ClickEncoder::Held ){
-          returnToMenu();
-      }
-    }   
-    
-  }
-}
-
-void kochen(){
-  sollTemp = 100;
-  lcd.clrScr();
-  lcd.print("--Kochen--",CENTER,0);
-  
-  int stateKochen = STATE_KOCHEN_KZ;
-  
-  while(state == STATE_KOCHEN){
-    switch(stateKochen){
-      case STATE_KOCHEN_KZ: //Einmaischtemp festlegen
-        break;
-      case STATE_KOCHEN_AHG: //Anzahl Hopfengaben festlegen
-        break;
-      case STATE_KOCHEN_HG: //Hopfengaben festlegen
-        break;
-      case STATE_KOCHEN_WT: //Warten auf Temperatur
-        break;
-      case STATE_KOCHEN_SKT: //Starte Kochtimer
-        break;
-      case STATE_KOCHEN_KT: //Starte Kochtimer
-        break;
-    } 
-    
-    ClickEncoder::Button b = encoder->getButton();
-    if (b != ClickEncoder::Open) {
-      if( b == ClickEncoder::Held ){
-          returnToMenu();
-      }
-    }   
-    
-  }  
-}
-
-void returnToMenu(){
-  sollTemp = 0;
-  selectedMenuEntry = 0;
-  lastSelectedMenuEntry = -1;
-  state = STATE_MENU; 
+void timerIsr() {
+  encoder->service();
 }
