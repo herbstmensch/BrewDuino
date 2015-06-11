0f70871db12e257cd825faffc121d829743f5e74
Tim Herbst <mail@timherbst.de>
2015-06-11 10:40:19 +0200

    Update BrewDuino.ino

--- BrewDuino.ino
+++ BrewDuino.ino
@@ -110,10 +110,10 @@ void setup()   {
   lcd.InitLCD();
   lcd.clrScr();
   lcd.setFont(SmallFont);
-  lcd.print(CREATOR, LEFT, 0);
-  lcd.print(PROG_NAME" "PROG_VERSION, LEFT, 16);
-  lcd.print(COPYRIGHT, LEFT, 32);
-  lcd.print(COPYRIGHT2, RIGHT, 40);
+  printRow()(CREATOR, LEFT, 0);
+  printRow()(PROG_NAME" "PROG_VERSION, LEFT, 16);
+  printRow()(COPYRIGHT, LEFT, 32);
+  printRow()(COPYRIGHT2, RIGHT, 40);
   
   #if SERIAL != N
     Serial.begin(9600);
@@ -174,7 +174,7 @@ void enterMenu(){
   setSollTemp(MIN_TEMP);
   checkHeatingStatus(true);
   clrScr(true,false);
-  lcd.print(PROG_NAME" "PROG_VERSION, CENTER, 0);
+  printRow()(PROG_NAME" "PROG_VERSION, CENTER, 0);
   forceFirstDisplay();
 }
 
@@ -186,7 +186,7 @@ void enterMaischen(){
   encoder->setAccelerationEnabled(true);
   fsmMaischen.immediateTransitionTo(stateEnterEinmaischTemp);
   clrScr(true,false);
-  lcd.print("--Maischen--",CENTER,0);
+  printRow()("--Maischen--",CENTER,0);
 }
 
 void leaveMaischen(){
@@ -199,7 +199,7 @@ void enterKochen(){
   checkHeatingStatus(true);
   fsmKochen.immediateTransitionTo(stateEnterKochzeit);
   clrScr(true,false);
-  lcd.print("--Kochen--",CENTER,0);
+  printRow()("--Kochen--",CENTER,0);
 }
 
 void leaveKochen(){
@@ -210,7 +210,7 @@ void enterHeizen(){
   encoder->setAccelerationEnabled(true);
   setSollTemp(temp);
   clrScr(true,false);
-  lcd.print("--Heizen--",CENTER,0);
+  printRow()("--Heizen--",CENTER,0);
   forceFirstDisplay();
 }
 
@@ -222,7 +222,7 @@ void enterTimer(){
   encoder->setAccelerationEnabled(true);
   fsmTimer.immediateTransitionTo(stateEnterTimerTime);
   clrScr(true,false);
-  lcd.print("--Timer--",CENTER,0);
+  printRow()("--Timer--",CENTER,0);
 }
 
 /*void enterSettings(){
@@ -241,13 +241,11 @@ void menu() {
       for(int i = encoderValue; i < 0; i++)
         menuDown();
         
-    clrScr(false,false);
-    
     if(selectedMenuEntry == 0+menuOffset)
-      lcd.print("=>", 10, 16);
+      printRow()("=>", 10, 16);
     lcd.print(menuEntrys[0+menuOffset], 25,16);
     if(selectedMenuEntry == 1+menuOffset)
-      lcd.print("=>", 10, 24);
+      printRow()("=>", 10, 24);
     lcd.print(menuEntrys[1+menuOffset], 25,24);
   }
   
@@ -378,7 +376,7 @@ void addTemperature(boolean force) {
     s += "~ C";
     char buf[14];
     s.toCharArray(buf,14);
-    lcd.print(buf, RIGHT, 40);
+    printRow()(buf, RIGHT, 40);
   }
 }
 
@@ -397,6 +395,13 @@ void clrScr(bool clearFirst, bool clearLast){
   if(clearLast){
     lcd.clrRow(5);
   }
+}
+
+void printRow(char* text, int pos, int row){
+  if(row > 5)
+    row = row / 8;
+  lcd.clrRow(row);
+  lcd.print(text, pos, row*8);
 }
 
 void timerIsr() {
@@ -449,9 +454,4 @@ void checkForErrors(){
     //Kein Thermometer
     errors += ERROR_NO_THERMOMETER;
   }	
-}
-
-void printRow(char* text, int pos, int row){
-    lcd.clrRow(row);
-    lcd.print(text, pos, row*8);
 }
