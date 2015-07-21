f19316db628186b6db752d605597c2cec23132d8
Tim Herbst <mail@timherbst.de>
2015-07-21 14:50:17 +0200

    Bilder vom Board hinzugefügt

--- Hardware/rename_gerber.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm *pnp.txt
-for i in Brew*.*; do mv "$i" "BrewDuino.${i##*.}"; done
-mv BrewDuino.gm1 BrewDuino.gko
-mv BrewDuino.txt BrewDuino.xln
-zip BrewDuino.zip Brew*.g*
-zip BrewDuino.zip Brew*.x*
